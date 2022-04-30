d4ef904e196be1ce25f91a5b39fd727ba4be1087
Alan Huang <98438095+a1an-huang@users.noreply.github.com>
2022-04-30 01:45:33 -0400

    pt1

--- cse220-hw5-a1an-huang/hw5.asm
+++ cse220-hw5-a1an-huang/hw5.asm
@@ -8,6 +8,24 @@
 .text:
 .globl create_term
 create_term:
+	move $t0, $a0
+	
+	beq $t0, $0, create_term_err 		#coefficent is 0
+	blt $a1, $0, create_term_err		#exponent less than 0
+	
+	li $a0, 12				#allocate mem for heap
+	li $v0, 9
+	syscall					#addr of allocated heap in v0
+	
+	sw $t0, 0($v0)				#store coefficent at addr
+	sw $a1, 4($v0)				#store exponent at addr + 4
+	sw $0, 8($v0)				#store 0 for last 4 bytes
+	j create_done
+	
+create_term_err:
+	li $v0, -1 
+	j create_done
+create_done:	
   jr $ra
 
 .globl create_polynomial
